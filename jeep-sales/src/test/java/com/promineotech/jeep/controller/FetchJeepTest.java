79ba08016fc650d000cf628888b2651c1dfec8fe
allebravo <alejanbravo@outlook.com>
2023-02-23 20:39:58 -0800

    added week 14 code

--- jeep-sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,9 +1,14 @@
 package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,6 +27,7 @@ import com.promineotech.jeep.entity.JeepModel;
     config = @SqlConfig(encoding = "utf-8"))
 
 class FetchJeepTest extends FetchJeepTestSupport{
+	
 
 	@Test
 	void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
@@ -32,12 +38,19 @@ class FetchJeepTest extends FetchJeepTestSupport{
 				String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);	
 		
 		//When: a connection is made to the URI
-		ResponseEntity<Jeep> response =
-				getRestTemplate().getForEntity(uri, Jeep.class);
+		ResponseEntity<List<Jeep>> response =
+				getRestTemplate().exchange(uri, HttpMethod.GET, null,
+						new ParameterizedTypeReference<>() {});
 		
 		//Then : a success (OK-200) status code is returned.
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 		
+		//And: the actual list returned is the same as the expected list
+		List<Jeep> expected = buildExpected ();
+		assertThat(response.getBody()).isEqualTo(expected);
+		
 	}
+
+
 
 }
