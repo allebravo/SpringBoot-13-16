7e2e464259e92640e2755059deed31cc6c1013a1
allebravo <alejanbravo@outlook.com>
2023-03-04 22:15:55 -0800

    added week 15 code

--- jeep-sales/src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
@@ -1,8 +1,15 @@
 package com.promineotech.jeep.controller.support;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
@@ -10,6 +17,17 @@ public class FetchJeepTestSupport extends BaseTest{
 	protected List<Jeep> buildExpected() {
 		List<Jeep>	list = new LinkedList<>();
 		//@formatter : off
+		
+
+		
+		list.add(Jeep.builder()
+				.modelId(JeepModel.WRANGLER)
+				.trimLevel("Sport")
+				.numDoors(4)
+				.wheelSize(17)
+				.basePrice(new BigDecimal("31975.00"))
+				.build());
+		
 		
 		list.add(Jeep.builder()
 				.modelId(JeepModel.WRANGLER)
@@ -19,15 +37,46 @@ public class FetchJeepTestSupport extends BaseTest{
 				.basePrice(new BigDecimal("28475.00"))
 				.build());
 		
-		list.add(Jeep.builder()
-				.modelId(JeepModel.WRANGLER)
-				.trimLevel("Sport")
-				.numDoors(4)
-				.wheelSize(17)
-				.basePrice(new BigDecimal("31975.00"))
-				.build());
+		
 		//@formatter : on
+		
+		Collections.sort(list);
 		return list;
 	}
+	/**
+	 * 
+	 * @param error
+	 * @param status
+	 */
+
+		protected void assertErrorMessage(Map<String, Object> error,
+				HttpStatus status) {
+			//@formatter: off
+			assertThat(error)
+			.containsKey("message")
+			.containsEntry("status code", status.value())
+			.containsEntry("uri", "/jeeps")
+			.containsKey("timestamp")
+			.containsEntry("reason",status.getReasonPhrase());
+			//@formatter:on
+		}
+
+	/**
+	 * 
+	 * @param error
+	 * @param status
+	 */
+
+		protected void assertErrorMessageValid(Map<String, Object> error,
+				HttpStatus status) {
+			//@formatter: off
+			assertThat(error)
+			.containsKey("message")
+			.containsEntry("status code", status.value())
+			.containsEntry("uri", "/jeeps")
+			.containsKey("timestamp")
+			.containsEntry("reason",status.getReasonPhrase());
+			//@formatter:on
+		}
 
 }
